b2c448d9b6347574fc6cbb6cc35179a36bba3ba6
Silvian Cretu <silvian.cretu@8x8.com>
2020-07-05 14:44:38 +0300

    express-helloworld: added Oana functionality

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -9,6 +9,10 @@ app.get('/mars', function (req, res) {
   res.send('Hello Mars!\n');
 });
 
+app.get('/oana', function (req, res) {
+  res.send('Te iubesc! &#10084;\n');
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
